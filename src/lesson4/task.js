8c9fb86d8342c60ed2da840d2487e97ddbe761d7
Maxim Karapysh <karapysh15@gmail.com>
2018-02-13 01:42:42 +0200

    hw to fourth lesson

--- src/lesson4/task.js
+++ src/lesson4/task.js
@@ -8,8 +8,46 @@
   in forEach() method.
   mySet = createSet(['a', 'b', 'c'])
 */
-export function createSet(arr) {
 
+class CustomSet {
+  constructor(arr = []) {
+    this.value = arr;
+  }
+
+  get size() {
+    return this.value.length;
+  }
+
+  add(item) {
+    return this.has(item) ? this.value : this.value.push(item);
+  }
+
+  clear() {
+    return this.value = [];
+  }
+
+  has(item) {
+    return this.value.includes(item);
+  }
+
+  ['delete'](item) {
+    if (this.has(item)) {
+      this.value = this.value.filter(currentVal => currentVal != item);
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  forEach(cb, thisArg = this) {
+    thisArg.value.forEach((value, index, array) => {
+      cb(value, value, array);
+    });
+  }
+}
+
+export function createSet(arr) {
+  return new CustomSet(arr);
 }
 
 /*
@@ -22,8 +60,62 @@ export function createSet(arr) {
   in forEach() method.
   myMap = createMap([['a', 1], ['b', 2], ['c', 3]])
 */
-export function createMap(arr) {
 
+
+class CustomMap {
+  constructor(arr = []) {
+    this.value = arr;
+  }
+
+  get size() {
+    return this.value.length;
+  }
+
+  set(key, value) {
+    if (this.has(key)) {
+      this.value.forEach(item => {
+        if (item.includes(key)) {
+          item[1] = value;
+        };
+      });
+      return this.value;
+    } else {
+      return this.value.push([key, value]);
+    }
+  }
+
+  clear() {
+    return this.value = [];
+  }
+
+  has(key) {
+    return this.value.some(item => item.includes(key));
+  }
+
+  get(key) {
+    let result = this.value.find(item => item[0] === key);
+    return result ? result[1] : false;
+  }
+
+  ['delete'](key) {
+    if (this.has(key)) {
+      this.value = this.value.filter(item => !(item[0] === key));
+      return true;
+    }
+    return false;
+
+  }
+
+  forEach(cb, thisArg = this) {
+    thisArg.value.forEach((element, index, arr) => {
+      let [key, value] = element;
+      cb(value, key, arr);
+    })
+  }
+}
+
+export function createMap(arr) {
+  return new CustomMap(arr);
 }
 
 export default {
