134681c9ab6598d9ee9e8d09bea70e11d05d65c1
Maxim Karapysh <karapysh15@gmail.com>
2018-02-16 02:30:39 +0200

    hw to sixth lesson without testing

--- src/lesson6/task.js
+++ src/lesson6/task.js
@@ -2,23 +2,49 @@
 
 // Климат на любой планете по её имени
 // {planetName} – String
-const getClimate = function(planetName) {
+const getClimate = function (planetName) {
+  const URL = 'https://swapi.co/api/planets/?search=';
+
+  return fetch(`${URL}${planetName}`)
+    .then(response => response.json())
+    .then(data => data.results[0].climate)
+    .catch(err => { throw err });
 
 }
 
 // Получить информацию (Object) о любом персонаже по имени
 // {name} – String
-const getProfile = function(name) {
-
+const getProfile = function (name) {
+  const URL = 'https://swapi.co/api/people/?search=';
+  
+  return fetch(`${URL}${name}`)
+    .then(response => response.json())
+    .then(data => {
+      if (!data.results[0]) {
+        throw new Error('Non existent character');
+      }
+      return data.results[0];
+    })
 }
-
 // Получить список пилотов (имена в виде Array of Strings) космического корабля
 // по его названию
 // {starshipName} - String
-const getPilots = function(starshipName) {
+const getPilots = function (starshipName) {
 
-}
+  const URL = 'https://swapi.co/api/starships/?search=';
 
+  return fetch(`${URL}${starshipName}`)
+    .then(response => response.json())
+    .then(data => data.results[0].pilots)
+    .then(pilots => {
+      return Promise.all(pilots.map(pilot => {
+        return fetch(pilot)
+          .then(response => response.json())
+          .then(item => item.name)
+      }))
+    })
+    .catch(err => { throw err });
+};
 
 export default {
   getClimate,
