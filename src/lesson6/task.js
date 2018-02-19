efc1cb8e3e31e084ffc2de0492f7affd07d5a315
Maxim Karapysh <karapysh15@gmail.com>
2018-02-19 16:33:27 +0200

    refactoring hw6

--- src/lesson6/task.js
+++ src/lesson6/task.js
@@ -2,48 +2,61 @@
 
 // Климат на любой планете по её имени
 // {planetName} – String
-const getClimate = function (planetName) {
+const getClimate = async function (planetName) {
   const URL = 'https://swapi.co/api/planets/?search=';
 
-  return fetch(`${URL}${planetName}`)
-    .then(response => response.json())
-    .then(data => data.results[0].climate)
-    .catch(err => { throw err });
-
+  try {
+    let response = await fetch(`${URL}${planetName}`);
+    let data = await response.json();
+    return data.results[0].climate
+  } catch (error) {
+    throw new Error(error)
+  }
 }
 
 // Получить информацию (Object) о любом персонаже по имени
 // {name} – String
-const getProfile = function (name) {
+
+const getProfile = async function (name) {
   const URL = 'https://swapi.co/api/people/?search=';
-  
-  return fetch(`${URL}${name}`)
-    .then(response => response.json())
-    .then(data => {
-      if (!data.results[0]) {
-        throw new Error('Non existent character');
-      }
-      return data.results[0];
-    })
+
+  try {
+    let response = await fetch(`${URL}${name}`);
+    let data = await response.json();
+
+    if (!data.results[0]) {
+      throw new Error('Non existent character');
+    }
+
+    return data.results[0];
+  } catch (error) {
+    throw new Error(error)
+  }
 }
+
 // Получить список пилотов (имена в виде Array of Strings) космического корабля
 // по его названию
 // {starshipName} - String
-const getPilots = function (starshipName) {
 
+const getPilots = async function (starshipName) {
   const URL = 'https://swapi.co/api/starships/?search=';
 
-  return fetch(`${URL}${starshipName}`)
-    .then(response => response.json())
-    .then(data => data.results[0].pilots)
-    .then(pilots => {
-      return Promise.all(pilots.map(pilot => {
-        return fetch(pilot)
-          .then(response => response.json())
-          .then(item => item.name)
-      }))
-    })
-    .catch(err => { throw err });
+  try {
+    let response = await fetch(`${URL}${starshipName}`);
+    let data = await response.json();
+
+    if (data.count === 0) {
+      throw new Error('non existent such starship');
+    }
+
+    return Promise.all(data.results[0].pilots.map(async (pilot) => {
+      let response = await fetch(pilot);
+      let data = await response.json();
+      return data.name;
+    }))
+  } catch (error) {
+    throw new Error(error)
+  }
 };
 
 export default {
